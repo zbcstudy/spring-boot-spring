ac5b2c1fd69a45aeb1e2a120bccb30f92db426fb
Hasee <1434756304@qq.com>
2019-01-07 23:20:34 +0800

    netty4实现

---
+++ src/main/java/com/wondertek/self/spring/netty4/Netty4Handler.java
@@ -0,0 +1,21 @@
+package com.wondertek.self.spring.netty4;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class Netty4Handler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+    }
+}
