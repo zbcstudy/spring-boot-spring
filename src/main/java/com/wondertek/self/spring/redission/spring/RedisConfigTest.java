36337994902f68c7386e1e164d8b31aeaee5bd54
Hasee <1434756304@qq.com>
2019-01-29 23:20:22 +0800

    测试代码修改

--- src/main/java/com/wondertek/self/spring/redission/spring/RedisConfigTest.java
+++ src/main/java/com/wondertek/self/spring/redission/spring/RedisConfigTest.java
@@ -2,6 +2,7 @@ package com.wondertek.self.spring.redission.spring;
 
 import org.junit.Test;
 import org.redisson.RedissonClient;
+import org.redisson.core.RAtomicLong;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,6 +15,17 @@ public class RedisConfigTest {
     public void testRedisson() {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RedissonConfig.class);
         RedissonClient redissonClient = applicationContext.getBean(RedissonClient.class);
-        System.out.println(redissonClient);
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                RAtomicLong atomicLong = redissonClient.getAtomicLong("spring:redis:lock");
+                atomicLong.incrementAndGet();
+            }).start();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(redissonClient.getAtomicLong("spring:redis:lock").get());
     }
 }
